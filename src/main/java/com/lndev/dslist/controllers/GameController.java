d422accbd4381548eb379f3d41b2b832c1e4ec2c
Luiz Almeida <l.n.almeida.ti@gmail.com>
2023-07-07 23:36:26 -0300

    Updated GameService & GameController

--- src/main/java/com/lndev/dslist/controllers/GameController.java
+++ src/main/java/com/lndev/dslist/controllers/GameController.java
@@ -1,12 +1,16 @@
 package com.lndev.dslist.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lndev.dslist.dto.GameDTO;
 import com.lndev.dslist.dto.GameMinDTO;
 import com.lndev.dslist.services.GameService;
 
@@ -21,5 +25,15 @@ public class GameController {
 	public List<GameMinDTO> findAllGames(){
 		List<GameMinDTO> result = gameService.findAllGames();
 		return result;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<GameDTO> findGameById(@PathVariable Long id) {
+		Optional<GameDTO> result = gameService.findGameById(id);
+		if(result.isPresent()) {
+			return ResponseEntity.ok(result.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
--- src/main/java/com/lndev/dslist/services/GameService.java
+++ src/main/java/com/lndev/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.lndev.dslist.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lndev.dslist.dto.GameDTO;
 import com.lndev.dslist.dto.GameMinDTO;
 import com.lndev.dslist.entities.Game;
 import com.lndev.dslist.repositories.GameRepository;
@@ -17,7 +20,11 @@ public class GameService {
 	
 	public List<GameMinDTO> findAllGames(){
 		List<Game> result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		return result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
 		
 	}
+	
+	public Optional<GameDTO> findGameById(Long id) {
+		return gameRepository.findById(id).map(GameDTO::new);
+	} 
 }
